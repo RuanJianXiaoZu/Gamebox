12137b27665016b8ebcb1aa66d1066cf13ac9eb2
wy2981 <1446589557@qq.com>
2021-05-31 17:39:22 +0800

    fix main

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ os.chdir(dirname)
 sg.theme('Default 1')
 layout = [
     [sg.Text("Welcome! Select a game below: ")],
-    [sg.Button('ChineseChess', enable_events=True)],
-    [sg.Button('link game', enable_events=True)],
-    [sg.Button('Minesweeper', enable_events=True)],
-    [sg.Button('Puzzle', enable_events=True)],
-    [sg.Button('Sokoban', enable_events=True)],
-    [sg.Button('Gobang', enable_events=True)],
-    [sg.Button('Exit', enable_events=True)]
+    [sg.Button('象棋单机游戏', enable_events=True)],
+    [sg.Button('象棋联机游戏', enable_events=True)],
+    [sg.Button('连连看', enable_events=True)],
+    [sg.Button('扫雷', enable_events=True)],
+    [sg.Button('拼图', enable_events=True)],
+    [sg.Button('推箱子', enable_events=True)],
+    [sg.Button('五子棋', enable_events=True)],
+    [sg.Button('退出', enable_events=True)]
 ]
 window = sg.Window('GameBox', layout, finalize=True)
 window.Size = (400, 300)
@@ -22,19 +23,21 @@ window.Size = (400, 300)
 while True:
     event, values = window.read()
     print(event, values)
-    if event == 'ChineseChess':
-        os.system('python ./ChineseChess/game.py')
-    if event == 'link game':
+    if event == '象棋单机游戏':
+        os.system('python ./ChineseChess/main.py')
+    if event == '象棋联机游戏':
+        os.system('python ./ChineseChess/client.py')
+    if event == '连连看':
         os.system('python ./link_game/main.py')
-    if event == 'Minesweeper':
+    if event == '扫雷':
         os.system('python ./Minesweeper/main.py')
-    if event == 'Puzzle':
+    if event == '拼图':
         os.system('python ./Puzzle/game.py')
-    if event == 'Sokoban':
+    if event == '推箱子':
         os.system('python ./Sokoban/game.py')
-    if event == 'Gobang':
+    if event == '五子棋':
         os.system('python ./Gobang/main.py')
-    if event == 'Exit' or event == sg.WINDOW_CLOSED:
+    if event == '退出' or event == sg.WINDOW_CLOSED:
         break   
 
 window.close()
